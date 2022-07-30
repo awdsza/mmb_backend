53d961ebbfd8b034ed7368bd1444134e5833d5e3
awdsza <37355034+awdsza@users.noreply.github.com>
2022-07-30 15:47:08 +0900

    feat:사용자서비스API를구현하기위한 파일추가

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Post, Controller, Param, Get, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserInfo } from './UserInfo';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UsersService } from './users.service';
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    const { name, email, password } = dto;
+    await this.usersService.createUser(name, email, password);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    return this.usersService.getUserInfo(userId);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { UserInfo } from './UserInfo';
+import * as uuid from 'uuid';
+@Injectable()
+export class UsersService {
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    //await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  async login(email: string, password: string): Promise<string> {
+    // TODO
+    // 1. email, password를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러 처리
+    // 2. JWT를 발급
+
+    throw new Error('Method not implemented.');
+  }
+  async getUserInfo(userId: string): Promise<UserInfo> {
+    // 1. userId를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러 처리
+    // 2. 조회된 데이터를 UserInfo 타입으로 응답
+
+    throw new Error('Method not implemented.');
+  }
+}
