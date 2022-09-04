ceb532f5af589d554e5ef3d0e7b636b3640ec866
awdsza <37355034+awdsza@users.noreply.github.com>
2022-09-05 00:23:58 +0900

    chord:express-session패키지 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+      ? '.stage.env'
+      : '.development.env',
+  ),
+});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 }
 bootstrap();
