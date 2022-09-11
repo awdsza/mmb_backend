e9126251539f2d96815ed971f2ca11d6dc7e4312
awdsza <37355034+awdsza@users.noreply.github.com>
2022-09-12 02:28:45 +0900

    feat:카테고리관련작업전에JWT인증 추가.

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -9,28 +9,36 @@ import {
   UnauthorizedException,
   InternalServerErrorException,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryBaseDto } from './dto/category-base.dto';
+import { GetUser } from 'src/auth/user-decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { UserEntity } from 'src/users/entity/users.entity';
 @Controller('category')
+@UseGuards(AuthGuard('jwt'))
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
-  @Get('/:userSeq/:inOutType')
+  @Get('/:inOutType')
   async getCategories(
+    @GetUser() user: UserEntity,
     @Param('inOutType') inOutType: string,
-    @Param('userSeq') userSeq: number,
   ): Promise<CategoryBaseDto[]> {
     try {
+      const { userSeq } = user;
       return await this.categoryService.getCategories(inOutType, userSeq);
     } catch (e) {}
   }
 
   @Post()
   async saveCategory(
+    @GetUser() user: UserEntity,
     @Body() categoryParamDto: CategoryBaseDto,
   ): Promise<object> {
-    const { inOutType, userSeq, categoryName } = categoryParamDto;
+    const { inOutType, categoryName } = categoryParamDto;
     try {
+      const { userSeq } = user;
       const result = await this.categoryService.saveCategory(
         inOutType,
         userSeq,
@@ -45,8 +53,11 @@ export class CategoryController {
       return { ...e, isSuccess: false };
     }
   }
-  @Get('/:seq')
-  async getCategory(@Param('seq') seq: number): Promise<CategoryBaseDto> {
+  @Get('/:inOutType/:seq')
+  async getCategory(
+    @Param('inOutType') inOutType: string,
+    @Param('seq') seq: number,
+  ): Promise<CategoryBaseDto> {
     try {
       return await this.categoryService.getCategory(seq);
     } catch (e) {}
@@ -71,13 +82,14 @@ export class CategoryController {
       return { ...e, isSuccess: false };
     }
   }
-  @Put('/sort/:userseq/:inouttype')
+  @Put('/sort/:inouttype')
   async updateCategoriesSort(
-    @Param('userseq') userSeq: number,
+    @GetUser() user: UserEntity,
     @Param('inouttpye') inOutType: string,
     @Body() categoryParamDto: CategoryBaseDto[],
   ): Promise<object> {
     try {
+      const { userSeq } = user;
       const result = await this.categoryService.updateCategoriesSort(
         userSeq,
         inOutType,
